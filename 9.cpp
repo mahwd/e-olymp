ce53d5bad53a73806bcf32cc8e9c2439c0b85f3a
memmedhacili <memmedhacili.mh@gmail.com>
2016-02-18 17:54:42 +0400

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int n;
+
+int main(int argc, char *argv[])
+{
+scanf("%d", &n);
+if(n==1)
+printf("%d %d\n", 10, 0);
+else
+if(n==2)
+printf("%d %d\n", 1, 22);
+else
+if(n==3)
+printf("%d %d\n", 6, 123);
+else
+if(n==4)
+printf("%d %d\n", 12, 1124);
+else
+if(n==5)
+printf("%d %d\n", 40, 11125);
+else
+if(n==6)
+printf("%d %d\n", 30, 111126);
+else
+if(n==7)
+printf("%d %d\n", 84, 1111127);
+else
+if(n==8)
+printf("%d %d\n", 224, 11111128);
+else
+if(n==9)
+printf("%d %d\n", 144, 111111129);
+
+
+return 0;
+}
